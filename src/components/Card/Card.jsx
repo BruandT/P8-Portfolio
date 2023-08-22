bb0b6262bf65246c8596a1ec268c217bc5209575
Thomas Bruand <thomas.bruand@gmail.com>
2023-08-22 12:05:07 +0200

    Ajout de style (Z-index) sur les elements de la modale

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,50 @@ import Modal from "react-modal";
 
 const customStyles = {
   content: {
-    width: "65%",
-    height: "80%",
+    position: "absolute",
+    width: "95%",
+    height: "95%",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    background: "#141C1A",
+    zIndex: 999,
+  },
+  overlay: {
+    backgroundColor: "#00000052",
+    zIndex: 998, // Z-index pour l'overlay de la modale
+  },
+  modalContent: {
+    position: "relative", // Modifiez cette ligne pour permettre la gestion de z-index
+    zIndex: 1000, // Z-index pour le contenu de la modale
+  },
+  banner: {
+    position: "relative", // Modifiez cette ligne pour permettre la gestion de z-index
+    zIndex: 1001, // Z-index pour la bannière
+  },
+  closeButton: {
+    position: "absolute",
+    top: "-15px",
+    right: "-10px",
+    zIndex: 1002, // Z-index pour le bouton de fermeture
   },
 };
+Modal.defaultStyles.overlay.backgroundColor = "#00000052";
+Modal.defaultStyles.content.backgroundColor = "#ffffff";
 
 Modal.setAppElement("#root");
 
-const Card = ({ post }) => {
+const Card = ({
+  images,
+  name,
+  title,
+  year,
+  description,
+  tags,
+  roadmap,
+  problems,
+  link,
+  video,
+}) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function openModal() {
@@ -28,69 +60,84 @@ const Card = ({ post }) => {
 
   return (
     <>
-      {/*Container de Card  */}
-      <div
-        className='card-overlay w-1/2 h-1/2 box-border p-3 rounded max-lg:w-full'
-        key={post.id}
-      >
+      {/* Container de Card  */}
+      <div className='h-80 max-sm:h-56 card-overlay box-border rounded'>
         <img
           className='card-image w-full h-full rounded'
-          src={`http://localhost:1337${post.attributes.card.data.attributes.url}`}
-          alt={post.attributes.card.data.attributes.name}
+          src={`http://localhost:1337${images}`}
+          alt={name}
         />
         <div
           onClick={openModal}
           className='card-hover w-full h-full flex flex-col items-center justify-center relative bottom-full rounded'
         >
-          <h2 className='text-xl'>{post.attributes.title}</h2>
-          <p className='text-xl relative top-44 left-48 text-green-dark max-lg:left-0 max-lg:top-20 '>
-            Voir plus &#10132;
-          </p>
+          <h2 className='text-4xl text-skin-color'>{title}</h2>
+          <div className='w-full h-8 relative top-28 left-0 max-xl:left-0 max-lg:top-28 max-sm:left-0 max-sm:top-16 flex justify-end'>
+            <p className='text-xl text-skin-color mr-5'>Voir plus &#10132;</p>
+          </div>
         </div>
         {/* ouverture de la modale */}
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
           style={customStyles}
-          contentLabel={`Modal ${post.attributes.title}`}
+          contentLabel={`Modal ${title}`}
         >
-          <div className='w-full h-full flex flex-col'>
+          <div
+            className='w-full h-full p-8 flex flex-col max-xl:p-0'
+            style={customStyles.modalContent}
+          >
+            {/* title */}
+            <h2 className='text-skin-base text-4xl max-sm:text-2xl'>{title}</h2>
             {/* div modal top */}
-            <div className='flex gap-3 mb-3 max-lg:flex-col'>
+            <div className='flex gap-3 max-xl:flex-col'>
               {/* div banner */}
-              <div className='w-1/2 mt-8 max-lg:w-full max-lg:mt-16'>
+              <div
+                className='w-1/2 flex mt-8 max-xl:w-full max-xl:mt-16 max-sm:mt-6'
+                style={customStyles.banner}
+              >
                 <img
-                  className='rounded'
-                  src={`http://localhost:1337${post.attributes.card.data.attributes.url}`}
+                  className='rounded xl:ml-5 w-5/6 max-xl:w-full'
+                  src={`http://localhost:1337${images}`}
                   alt=''
                 />
               </div>
               {/* div texte */}
-              <div className='text-white w-1/2 p-8 max-lg:w-full max-lg:p-0 max-lg:py-5'>
-                <h2 className='text-3xl mb-3 max-lg:text-4xl'>{post.attributes.title}</h2>
-                <div className="flex items-center mb-3">
-                <p className='text-xl'>Année: <span className="text-base">{post.attributes.year}</span></p>
-                </div>                
-                <h3 className='text-xl mb-2'>Description:</h3>
-                <p className='mb-3'>
-                  {post.attributes.description}
-                </p>
-               
+              <div className='text-skin-base w-1/2 p-8 max-xl:w-full max-xl:p-0 max-xl:py-5 max-sm:py-0'>
+                {/* div year */}
+                <div className='flex items-center mb-3'>
+                  <p className='text-xl max-sm:text-lg'>
+                    Année: <span className='text-base'>{year}</span>
+                  </p>
+                </div>
+                {/* div description */}
                 <div>
-                  <div>
-                    <p className='text-xl mb-2'>Technologies utilisées: </p>
-                    <div className='flex flex-wrap gap-3'>
-                    {post.attributes.tag1  ? <p className='border-2 text-green-dark rounded py-1 px-3'>{post.attributes.tag1}</p> : ' '}
-                    {post.attributes.tag2  ? <p className='border-2 text-green-dark rounded py-1 px-3'>{post.attributes.tag2}</p> : ' '}
-                    {post.attributes.tag3  ? <p className='border-2 text-green-dark rounded py-1 px-3'>{post.attributes.tag3}</p> : ' '}
-                    {post.attributes.tag4  ? <p className='border-2 text-green-dark rounded py-1 px-3'>{post.attributes.tag4}</p> : ' '}
-                    </div>
+                  <h3 className='text-xl mb-2 max-sm:text-lg'>Description:</h3>
+                  <p className='mb-3'>{description}</p>
+                </div>
+                {/* div tag */}
+                <div>
+                  <p className='text-xl max-sm:text-lg mb-2 max-sm:mb-0'>
+                    Technologies utilisées:{" "}
+                  </p>
+                  <div className='flex flex-wrap gap-3'>
+                    {tags.map((tag, index) => (
+                      <img
+                        key={index}
+                        className='w-16 h-16 m-2 max-sm:w-10 object-contain'
+                        src={`http://localhost:1337${tag.imageUrl}`}
+                        alt={`Logo ${tag.title}`}
+                      />
+                    ))}
                   </div>
                 </div>
                 {/* SVG pour fermer la modale */}
-                <button className='absolute top-5 right-5' onClick={closeModal}>
+                <button
+                  onClick={closeModal}
+                  style={customStyles.closeButton}
+                >
                   <svg
-                    className='h-10 w-10 text-green-dark max-lg:h-8 max-lg:w-8'
+                    className='h-10 w-10 text-skin-color max-xl:h-8 max-xl:w-8'
                     viewBox='0 0 24 24'
                     fill='none'
                     stroke='currentColor'
@@ -104,21 +151,55 @@ const Card = ({ post }) => {
                 </button>
               </div>
             </div>
+            <hr className='w-full my-10 bg-skin-bg-color' />
             {/* div modal bot */}
-            <div className='w-full flex justify-start flex-wrap gap-3 max-lg:justify-center'>
-              {Array.isArray(post.attributes.modal.data) &&
-              post.attributes.modal.data.length > 0 ? (
-                post.attributes.modal.data.map((image, index) => (
-                  <img
-                    key={index}
-                    className='image-w-modal object-fill rounded max-lg:w-full'
-                    src={`http://localhost:1337${image.attributes.url}`}
-                    alt={`Image du projet ${index + 1}`}
-                  />
-                ))
-              ) : (
-                <p>Aucune image disponible</p>
-              )}
+            <div className='w-full flex max-xl:flex-col-reverse justify-between'>
+              <div className='w-1/2 px-8 flex flex-col justify-between max-xl:w-full max-xl:px-0'>
+                <div className='w-full max-xl:mt-5'>
+                  <h3 className='text-2xl text-skin-base mb-4 '>
+                    Déroulement du projet :
+                  </h3>
+                  <p className='text-skin-base mb-3'>{roadmap}</p>
+                </div>
+                <div className='w-full max-xl:mt-5 max-xl:mb-5'>
+                  <h3 className='text-2xl text-skin-base mb-4'>
+                    Problématique :
+                  </h3>
+                  <div>
+                    {/* Problematique */}
+                    {problems ? (
+                      <ul className='mb-3'>
+                        {problems.split("+").map((problem, index) => (
+                          <li className='text-skin-base' key={index}>
+                            - {problem}
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p className='text-skin-base'>
+                        Aucune problématique disponible.
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className='w-1/2 flex flex-col-reverse justify-center max-xl:w-full'>
+                <div className='mt-3 w-full flex items-center'>
+                  <h4 className='text-lg text-skin-base'>Lien du projet :</h4>
+                  <a href={link} target='_blank' rel='noopener noreferrer'>
+                    <button className='ml-3 px-2 bg-skin-bg-color text-skin-light rounded'>                      
+                      Cliquez-ici !
+                    </button>
+                  </a>
+                </div>
+                <iframe
+                  className='w-full h-80 max-sm:h-40'
+                  src={video}
+                  title='YouTube video player'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                  allowFullScreen
+                ></iframe>
+              </div>
             </div>
           </div>
         </Modal>
