611f442fe62a6d31d66eeba7df7b4a7d953abb43
Thomas Bruand <thomas.bruand@gmail.com>
2023-08-22 12:00:59 +0200

    Ajout de l'api version fini

--- backend/src/api/social/routes/social.js
+++ backend/src/api/social/routes/social.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * image router
+ * social router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::image.image');
+module.exports = createCoreRouter('api::social.social');
--- backend/src/api/social/services/social.js
+++ backend/src/api/social/services/social.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * image service
+ * social service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::image.image');
+module.exports = createCoreService('api::social.social');
